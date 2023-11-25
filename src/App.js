e990984c2f4524743ef518d06582a8d7eee94ba5
JoachimHamraoui <joachim.hamraoui@student.ehb.be>
2023-11-25 12:16:17 +0100

    feature(dynamic-models): added useState const for changing model when needed

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import KoiFish from './models/Koi';
 import Madara from './models/Madara';
 import ShintoShrine from './models/Shinto';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+
+  const [selectModel, setSelectModel] = useState(null);
 
   const modelCollection = [
     {
@@ -29,9 +31,15 @@ const App = () => {
     },
   ]
 
+  const loadModel = (modelPath) => {
+    if (modelPath !== selectModel) {
+      setSelectModel(modelPath);
+    }
+  }
+
   return (
     <div>
-      <Navigation className='relative' modelCollection={modelCollection} />
+      <Navigation className='relative' modelCollection={modelCollection} loadModel={loadModel} />
      <CanvasComponent />
     </div>
   );
